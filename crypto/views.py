dbfb29a4421065c7eb0ed828f5493caf61fb3cd0
rupendr <rupendrarathod@gmail.com>
2020-01-05 23:28:27 +0530

    mysite

--- crypto/views.py
+++ crypto/views.py
@@ -4,7 +4,6 @@ import json
 
 # Create your views here.
 def home(request):
-    
     price_request=requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=XRP,BTC,BCH,ETH,EOS,XLM,LTC&tsyms=USD,INR")
     price=json.loads(price_request.content)
     api_request=requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
@@ -12,8 +11,6 @@ def home(request):
     return render(request,'home.html',{"api":api,"price":price})
 
 def prices(request):
-    
-    
     if request.method =="POST":
         quote=request.POST['quote']
         quote=quote.upper()
@@ -21,7 +18,7 @@ def prices(request):
         crypto=json.loads(crypto_request.content)
         return render(request,'prices.html',{"quote":quote,"crypto":crypto})
     else:
-        msg="please enter value"
+        msg="please enter price"
         return render(request,'prices.html',{"msg":msg})
 
 
